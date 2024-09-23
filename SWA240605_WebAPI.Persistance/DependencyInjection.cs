ef8ac82cacde3c91a19d2c3ad8ce16c337e7dd14
Maruthi BM <maruthi111099@gmail.com>
2024-09-23 17:20:52 +0530

    UPDATED IN DB CONTEXT

--- SWA240605_WebAPI.Persistance/DependencyInjection.cs
+++ SWA240605_WebAPI.Persistance/DependencyInjection.cs
@@ -34,24 +34,9 @@ namespace SWA240605_WebAPI.Persistance
             // entities
             //
             services.AddTransient<IApplicantRepository, ApplicantRepository>();
-            services.AddTransient<IApplicantDownloadRepository, ApplicantDownloadRepository>();
-            services.AddTransient<IApplicantEligibilityStatusRepository, ApplicantEligibilityStatusRepository>();
-            services.AddTransient<IApplicantUploadRepository, ApplicantUploadRepository>();
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IDistrictRepository, DistrictRepository>();
-            services.AddTransient<IDocumentTypeRepository, DocumentTypeRepository>();
-            services.AddTransient<IGenderRepository, GenderRepository>();
-            services.AddTransient<IIdentityCardTypeRepository, IdentityCardTypeRepository>();
-            services.AddTransient<IKannadaStudiedModeRepository, KannadaStudiedModeRepository>();
-            services.AddTransient<IPostHomePageContentRepository, PostHomePageContentRepository>();
-            services.AddTransient<IPostMenuRepository, PostMenuRepository>();
-            services.AddTransient<IPostNewsEventDocumentRepository, PostNewsEventDocumentRepository>();
-            services.AddTransient<IPostNewsEventRepository, PostNewsEventRepository>();
             services.AddTransient<IPostRepository, PostRepository>();
             services.AddTransient<IPostUnitRepository, PostUnitRepository>();
-            services.AddTransient<IPostUnitQualificationRepository, PostUnitQualificationRepository>();
-            services.AddTransient<IQualificationBoardRepository, QualificationBoardRepository>();
-            services.AddTransient<IRecruitmentActivityRepository, RecruitmentActivityRepository>();
             services.AddTransient<IUnionStateTerritoryRepository, UnionStateTerritoryRepository>();
             services.AddTransient<IVisitorRepository, VisitorRepository>();
 
